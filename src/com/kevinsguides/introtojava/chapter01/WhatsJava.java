9026ab7dbd26e6c845a9040fca953ad6b6826f8c
kevinolson93 <kevinolson93@gmail.com>
2021-10-16 12:02:48 -0400

    Initial commit

---
+++ src/com/kevinsguides/introtojava/chapter01/WhatsJava.java
@@ -0,0 +1,8 @@
+package com.kevinsguides.introtojava.chapter01;
+
+public class WhatsJava {
+    //We did not learn how to do any code in this lesson.
+    public static void main(String args[]){
+        System.out.println("We did not learn how to code in chapter one. Our first example starts with chapter two.");
+    }
+}
---
+++ src/com/kevinsguides/introtojava/chapter02/HelloWorld.java
@@ -0,0 +1,28 @@
+package com.kevinsguides.introtojava.chapter02;
+
+
+/**
+ * This Class file is related to the second chapter of the Intro to Java series at
+ * https://kevinsguides.com/guides/code/java/javaintro/02helloworld
+ * License: Public Domain
+ * @author: Kevin's Guides - Kevin Olson
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+
+        //print out using println method
+        System.out.println("Hello, World.");
+
+        //print a blank line with println
+        System.out.println();
+
+        //print using the printf method
+        System.out.printf("Hello, World.");
+
+        //print using the print method
+        System.out.print("Hello, World.");
+
+
+    }
+}
